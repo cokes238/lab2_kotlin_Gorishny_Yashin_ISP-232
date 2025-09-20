5462602bafdd50e1d685401ae0b1b119f86ff313
danil4237 <zdan2007@gmail.com>
2025-09-20 11:06:43 +0300

    Initial commit

---
+++ src/123.kt
@@ -0,0 +1,54 @@
+fun main() {
+//    println("Введите число")
+//    val number1= readln().toInt()
+//    println("Ввелите второе число ")
+//    val number2= readln().toInt()
+//    if (number1 > number2){
+//        println("Первое число больше")
+//    }
+//    else{
+//        println("второе число больше")
+//    }
+//    val max = if (number1 > number2)  number1 else number2
+//    println("Максимальное число $max")
+//    val age = readln().toInt()
+//    val status = if (age >= 18) "Совершеннолетний" else "Несовершеннолетний"
+//    println(status)
+//    println("Введите урон")
+//    val a = readln().toInt()
+//    println("Щит активен ? (введите да или нет)")
+//    val shield = readln() == "да"
+//    val res = if (shield) a / 2 else a
+//    println("Полученный урон $res")
+
+//    print("Введите ваш уровень: ")
+//    val level = readln().toInt()
+//    print("У вас есть VIP-статус? (true/false): ")
+//    val hasVIP = readln().toBoolean()
+//    if ((level >= 30 && hasVIP) || level >= 50) {
+//        println("Доступ разрешен!")
+//    } else {
+//        println("Доступ запрещен!")
+//    }
+
+//    val numbers = 1..100
+//    val symbol = 'a'..'z'
+
+//    print("Введите ваш уровень: ")
+//    val level = readln().toInt()
+//    if (level in 1..58) {
+//        println("Уровень $level cоответствует требованиям! Вы можете зайти в данж!")
+//    }else{
+//        println("Высокий уровень! Вход запрещен!")
+//    }
+
+
+
+
+
+
+
+}
+
+
+
---
+++ src/gdfgf.kt
@@ -0,0 +1,67 @@
+
+fun main() {
+
+//    val a: Int = 5
+//    val b: Float = 5.1f
+//    val c: Boolean = 5 == 6
+//    val d: Char = 'A'
+//    val e: String = "ngjf"
+//    val f: Any = 6453
+//    println(a)
+//    println(b)
+//    println(c)
+//    println(d)
+//    println(e)
+//    println(f)
+
+
+
+//val name "Danil"
+//    val age = 18 //Объявляем переменную с именем 'age' и присовить ей значение 18
+//    println("Hello") //Выводим 'Hello'
+//    println("My name is $name")//Выводим 'My name is Danil'
+//    println("My age is $age")//Выводим 'My age is 18'
+//    val a = 1 //Присваиваем переменной 'a' значение 1
+//    val b = 15//Присваиваем переменной 'b' значение 15
+//    val c = a + b + 8//Присваиваем пересенной 'c' значение (a + b + 8) и скалдываем числа из переменной 'a' и 'b', и прибавляем 8
+//    val str = c.toString()
+//    println(str)//Выводим значение переменной 'str'
+//    val numList = arrayOf(1,2,3)
+//    var x = 0//Присваиваем переменной 'x' значение 0
+//    while (x < 3) { //Cоздаем цикл с условием (х < 3)
+//        println ("Item $x is ${numList})") //Выводим 'Item 0 is '
+//        x = x + 1
+//    }
+
+
+
+//val a: Int = 7
+//val b: Double = 3.5
+//val integerb: Int = b.toInt()
+//println(a + integerb)
+//    println(a / integerb)
+
+
+
+//Исходный код
+//    var x: Int = 65.2
+//    var isPunk = true
+//    var message = 'Hello'
+//    isPunk = 10
+//    var y = 15
+//    y = y + "hello"
+
+//Проверенный код
+    var x: Float = 65.2f
+    var isPunk = true
+    var message = "Hello"
+    isPunk = 10 == 10
+    var y = "15"
+    y = y + "hello"
+
+
+
+
+
+
+}
